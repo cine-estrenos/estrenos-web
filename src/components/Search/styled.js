3644e68ce37ae88937e4b6668b4ef966d2363427
Leonardo Galante <lndgalante@gmail.com>
2019-12-27 20:23:07 -0300

    Add transition to input

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -15,11 +15,15 @@ export const Form = styled.form`
 export const Input = styled.input`
   all: unset;
   flex: 1;
+
   font-size: ${rem('16px')};
   padding: ${rem('18px')} ${rem('20px')};
+
+  transition: all 400ms ease;
   background-color: ${props => props.theme.grey};
 
   ::placeholder {
+    transition: all 400ms ease;
     color: ${props => rgba(props.theme.text, 0.3)};
   }
 `
