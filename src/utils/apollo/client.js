8f5fec04df50acf1d8c4f08ef978f73407275a14
Leonardo Galante <lndgalante@gmail.com>
2019-12-29 22:38:52 -0300

    Trigger deploy preview

--- src/utils/apollo/client.js
+++ src/utils/apollo/client.js
@@ -1,4 +1,4 @@
-import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
+import ApolloClient from 'apollo-boost'
 
 export const client = new ApolloClient({ uri: process.env.GATSBY_API_URL, fetch })
