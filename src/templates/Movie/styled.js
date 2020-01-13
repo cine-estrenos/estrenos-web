e4ac2821ed799a5573ba0a09db1ba76142c8982c
Leonardo Galante <lndgalante@gmail.com>
2020-01-12 21:02:22 -0300

    Display shows

--- src/templates/Movie/styled.js
+++ src/templates/Movie/styled.js
@@ -6,3 +6,13 @@ export const GoBack = styled.div`
   align-items: center;
   margin-bottom: ${rem('60px')};
 `;
+
+export const Header = styled.header`
+  display: grid;
+  grid-template-columns: ${rem('180px')};
+  margin: ${rem('20px')} 0;
+`;
+
+export const TableContainer = styled.div`
+  height: ${rem('400px')};
+`;
