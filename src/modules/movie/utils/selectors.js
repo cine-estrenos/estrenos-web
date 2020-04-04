d8cf60ea1ec2661f2a04ef10dc00232b1b88b375
Leonardo Galante <lndgalante@gmail.com>
2020-04-03 21:37:52 -0300

    Display cinemas in radio groups

--- src/modules/movie/utils/selectors.js
+++ src/modules/movie/utils/selectors.js
@@ -6,11 +6,21 @@ export const getAvailableCinemas = (cinemas, shows) => {
   return cinemas.filter((cinema) => shows.some(({ cinemaId }) => cinemaId === cinema.id));
 };
 
-export const getChainsNames = (cinemas) => {
-  return [...new Set(cinemas.map(({ chain }) => chain))];
+export const getCinemasChains = (cinemas) => {
+  const chainsSet = new Set(cinemas.map(({ chain }) => chain));
+  return Array.from(chainsSet);
+};
+
+export const getAvailableChains = (cinemas, shows) => {
+  const allCinemasChains = getCinemasChains(cinemas);
+  const availableCinemasChains = getCinemasChains(getAvailableCinemas(cinemas, shows));
+
+  return allCinemasChains
+    .map((chain) => ({ chain, isDisabled: !availableCinemasChains.includes(chain) }))
+    .sort((a, b) => b.chain.localeCompare(a.chain));
 };
 
 export const getAvailableBranches = (selectedCinema, availableCinemas) => {
   if (!selectedCinema) return [];
-  return availableCinemas.filter((cinema) => cinema.chain === selectedCinema[0].chain);
+  return availableCinemas.filter((cinema) => cinema.chain === selectedCinema);
 };
